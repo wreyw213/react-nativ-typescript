dce5f2e3ecdd295406ab3b4ee34816adcb82b984
sandeep singh <sandeepdhillon659@gmail.com>
2022-12-19 10:11:44 +0530

    unused code removd

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@
  */
 
 import React from 'react';
-import { SafeAreaView, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
+import { SafeAreaView, StatusBar, Text, useColorScheme, View } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -49,23 +49,5 @@ const App = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
