54dd734b56959ebb14d2e7b25a040f63c6259a05
sandeep singh <sandeepdhillon659@gmail.com>
2023-02-07 18:05:52 +0530

    firebase crashaltics and analytics for IOS implmented

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import DrawerStack from './src/navigation/DrawerStack';
+import analytics from '@react-native-firebase/analytics';
 
 
 
@@ -30,6 +31,8 @@ function DetailsScreen() {
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
   const Stack = createNativeStackNavigator();
+  const routeNameRef = React.useRef();
+  const navigationRef = React.useRef();
 
   return (
 
@@ -38,7 +41,22 @@ const App = () => {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={'black'}
       />
-      <NavigationContainer >
+      <NavigationContainer ref={navigationRef}
+        onReady={() => {
+          routeNameRef.current = navigationRef.current?.getCurrentRoute()?.name;
+        }}
+        onStateChange={async () => {
+          const previousRouteName = routeNameRef.current;
+          const currentRouteName = navigationRef.current?.getCurrentRoute()?.name;
+
+          if (previousRouteName !== currentRouteName) {
+            await analytics().logScreenView({
+              screen_name: currentRouteName,
+              screen_class: currentRouteName,
+            });
+          }
+          routeNameRef.current = currentRouteName;
+        }}>
         <Stack.Navigator screenOptions={{
           headerShown: false
         }}>
--- src/containers/UserInfoScreen/index.tsx
+++ src/containers/UserInfoScreen/index.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { DrawerScreenProps } from "@react-navigation/drawer";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Alert, ScrollView, Text } from "react-native";
+import { Alert, ListViewBase, ScrollView, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import styles from "./styles";
 import Button from "../../library/components/Button";
 import { LoginData } from "../../library/types/userInfo";
 import { loginApi } from "../../library/apis/userApis";
+import crashlytics from '@react-native-firebase/crashlytics';
+import analytics from '@react-native-firebase/analytics';
 
 type Props = NativeStackScreenProps<any> & DrawerScreenProps<any>
 
@@ -14,6 +16,7 @@ type Props = NativeStackScreenProps<any> & DrawerScreenProps<any>
 const UserInfoScreen: React.FC<Props> = ({ navigation }) => {
 
     const [userData, setUserData] = useState<LoginData>()
+    const [data, setData] = useState(null)
 
     const handleLogin = (): void => {
         const apiData = {
@@ -30,12 +33,53 @@ const UserInfoScreen: React.FC<Props> = ({ navigation }) => {
         })
     }
 
+    async function onSignIn(user: any) {
+
+        await analytics().logEvent('basket', {
+            id: 3745092,
+            item: 'mens grey t-shirt',
+            description: ['round neck', 'long sleeved'],
+            size: 'L',
+        })
+        return
+        try {
+            // crashlytics().crash()
+            console.log("----------_____________-----------");
+            crashlytics().log('User signed in.');
+            // crashlytics().recordError(new Error("app just stareted crashing"));
+            await Promise.all([
+                crashlytics().setUserId(user.uid),
+                crashlytics().setAttribute('credits', String(user.credits)),
+                crashlytics().setAttributes({
+                    role: 'admin',
+                    followers: '13',
+                    email: user.email,
+                    username: user.username,
+                }),
+            ]);
+            // const p = data.app.debug() //crash Here
+            console.log("p=>>>>>>>>>", p)
+        } catch (err: any) {
+            console.log("error", err);
+
+            crashlytics().recordError(err)
+        }
+    }
+
     return <SafeAreaView style={{ flex: 1 }}>
         <ScrollView style={{ flex: 1, }}>
 
             <Button
                 title="Login"
-                onPress={handleLogin}
+                // onPress={handleLogin}
+                onPress={() =>
+                    onSignIn({
+                        uid: 'Aa0Bb1Cc2Dd3Ee4Ff5Gg6Hh7Ii8Jj9',
+                        username: 'Joaquin Phoenix',
+                        email: 'phoenix@example.com',
+                        credits: 42,
+                    })
+                }
                 containerStyle={styles.buttonContainer}
                 textStyle={styles.buttonText}
             />
